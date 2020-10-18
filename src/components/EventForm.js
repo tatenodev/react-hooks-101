bc4b5e0588e19805cb3de30b5d4d3b6912015aeb
wd30gsrc <gd28.c.tateno@gmail.com>
2020-10-18 12:33:13 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,9 +2,12 @@ import React, { useContext, useState} from 'react'
 
 import {
     CREATE_EVENT,
-    DELETE_ALL_EVENTS
+    DELETE_ALL_EVENTS,
+    ADD_OPERATION_LOG,
+    DELETE_OPERATION_LOGS
 } from '../actions'
 import AppContext from '../contexts/AppContext'
+import {timeCurrentIso8601} from '../utils'
 
 const EventForm = () => {
     const {state, dispatch} = useContext(AppContext)
@@ -14,9 +17,15 @@ const EventForm = () => {
     const addEvent = e => {
         e.preventDefault()
         dispatch({
-        type: CREATE_EVENT,
-        title,
-        body
+            type: CREATE_EVENT,
+            title,
+            body
+        })
+
+        dispatch({
+            type: ADD_OPERATION_LOG,
+            description: 'イベントを作成しました。',
+            operatedAt: timeCurrentIso8601()
         })
 
         setTitle('')
@@ -26,7 +35,15 @@ const EventForm = () => {
     const deleteAllEvents = e => {
         e.preventDefault()
         const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-        if(result) dispatch({type: DELETE_ALL_EVENTS})
+        if(result) {
+            dispatch({type: DELETE_ALL_EVENTS})
+
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: '全てのイベントを削除しました。',
+                operatedAt: timeCurrentIso8601()
+            })
+        }
     }
 
     const unCreatable = title === '' || body === ''
